781e19dec6fdaccdaa054cb74fe6b47a274c1f48
Alice Mugure <machariamugure17@gmail.com>
2025-03-13 22:24:31 +0300

    New Data Added

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let drivers =['Antonio', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(drivers){
+   return drivers.slice(-2);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(Multiplier){
+    return  function(fare){
+        return fare * Multiplier;
+    } ;
+};
+
+const fareDoubler = function(fare){
+    return fare * 2;
+    
+};
+
+const fareTripler = function(fare){
+    return fare * 3;
+};
+
+const selectDifferentDrivers = function(drivers, driverFunction) {
+    return driverFunction(drivers);  
+  };
+
